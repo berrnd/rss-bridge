36d7ac93eb0e2046563c51fe3310e95a9c89f7c2
Bernd Bestel <bernd@berrnd.de>
2018-05-01 17:26:33 +0200

    Fixed AugsburgerAllgemeineBridge (site changed)

--- bridges/AugsburgerAllgemeineBridge.php
+++ bridges/AugsburgerAllgemeineBridge.php
@@ -3,7 +3,7 @@
 class AugsburgerAllgemeineBridge extends BridgeAbstract
 {
 	const NAME = 'Augsburger Allgemeine';
-	const URI = 'http://www.augsburger-allgemeine.de/';
+	const URI = 'https://www.augsburger-allgemeine.de/';
 	const DESCRIPTION = 'Returns search results from www.augsburger-allgemeine.de';
 	const MAINTAINER = 'bernd@berrnd.de';
 
@@ -40,27 +40,29 @@ class AugsburgerAllgemeineBridge extends BridgeAbstract
 
 	private function CollectBySearch()
 	{
-		$url = self::URI . 'suche/?num=20&q=' . urlencode($this->getInput('searchterm'));
-		$page = getSimpleHTMLDOMCached($url) or returnServerError('Error requesting ' . $url);
+		$url = self::URI . 'suche/?q=' . urlencode($this->getInput('searchterm'));
+		$searchResultsPage = getSimpleHTMLDOMCached($url) or returnServerError('Error requesting ' . $url);
 
-		foreach($page->find('.search-result-item') as $searchResultDiv)
+		foreach($searchResultsPage->find('a[class=p-swatch_topdown-nice p-blocklink p-block p-marg_v-xl]') as $searchResultElement)
 		{
+			$articleUrl = $searchResultElement->getAttribute('href');
+			$articlePage = getSimpleHTMLDOMCached($articleUrl) or returnServerError('Error requesting ' . $articleUrl);
+			
 			$item = array();
+			$item['uri'] = $articleUrl;
+			$item['title'] = $searchResultElement->getAttribute('title');			
+			$item['timestamp'] = strtotime($articlePage->find('div[class=p-fnt_std-normal--xs p-marg_t-xl p-float_right]', 0)->innertext);
+			$item['content'] = $articlePage->find('p[class=p-fnt_std-bold--ml]', 0)->innertext;
 
-			$item['uri'] = $searchResultDiv->find('.result-item-title', 0)->children(0)->href;
-			$item['title'] = $searchResultDiv->find('.result-item-title', 0)->children(0)->children(0)->innertext;
-			$item['timestamp'] = strtotime($searchResultDiv->find('.result-item-date', 0)->innertext);
-			$item['content'] = $searchResultDiv->outertext;
-
-			$authorElement = $searchResultDiv->find('.result-item-author', 0);
+			$authorElement = $articlePage->find('a[class=b-cms-text__no-styles p-line-height--m]', 0);
 			if ($authorElement !== null)
 			{
-				$item['author'] = $authorElement->innertext;
+				$item['author'] = $authorElement->getAttribute('title');
 			}
 
 			$this->items[] = $item;
 		}
 
-		$page->clear();
+		$searchResultsPage->clear();
 	}
 }
